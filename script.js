7a21ab2c3ed1dc4a6da98b8a27ad0ae89002cb4a
geethikavaishnavi <geethikavaishnavikonde@gmail.com>
2025-02-02 21:24:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ const recipeContainer = document.querySelector('.recipe-container');
 
 
 const fetchRecipes = async (query) =>{
+    recipeContainer.innerHTML ="<h2>Fetching Recipes...</h2>";
     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
     const response = await data.json();
 
+    recipeContainer.innerHTML ="";
     response.meals.forEach(meal => {
        const recipeDiv=document.createElement('div');
        recipeDiv.classList.add('recipe');
-       recipeDiv.innerHTML= `<img src="${meal.strMealThumb}">`
+       recipeDiv.innerHTML= `
+                <img src="${meal.strMealThumb}">
+                <h3>${meal.strMeal}</h3>
+                <p>${meal.strArea}</p>
+                <p>${meal.strCategory}</p>
+       `
        recipeContainer.appendChild(recipeDiv);
     });
     // console.log(response.meals[0]);
